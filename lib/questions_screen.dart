26fa9deea1f0ce1ac0b815bc3f16611772450548
VIA2NEY <kouameangeV11@gmail.com>
2024-07-04 18:07:45 +0000

    mapping questions items and styling

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -11,33 +12,38 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
+
+    final currentQuestion = questions;
+
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "The question ...",
-            style: TextStyle(
-              color: Colors.white
-            ),
-          ),
-          const SizedBox(height: 30),
-          
-          AnswerButton(
-            answerText: 'Answer 1',
-            onTap: (){},
-          ),
-          AnswerButton(
-            answerText: 'Answer 2',
-            onTap: (){},
-          ),
-          AnswerButton(
-            answerText: 'Answer 3',
-            onTap: (){},
-          ),
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
 
-        ],
+            Text(
+              currentQuestion[0].text,
+              style: const TextStyle(
+                color: Colors.white
+              ),
+              textAlign: TextAlign.center,
+            ),
+            
+            const SizedBox(height: 30),
+        
+            ... currentQuestion[0].answers.map((item) {
+                return AnswerButton(
+                  answerText: item,
+                  onTap: (){},
+                );
+              },
+            ),
+        
+          ],
+        ),
       ),
     );
   }
